ffe04caabb1071f8aa5c2240a104bfd83b1f3715
Sky1sBloo <ultimate8gaming@gmail.com>
2023-11-22 01:09:47 +0800

    Added print dividers

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ void decimalToBase(long double value, int base, int decimalPrecision = 8)
 
 	// Reverse to get the correct answer
 	std::reverse(answer.begin(), answer.end());
-	std::cout << answer << std::endl;
+	std::cout << "Answer Int: " <<  answer << std::endl;
 
 	// The ff code is for the decimal part of the value
 	long double decimal = value - std::floor(value);
@@ -60,27 +60,40 @@ void decimalToBase(long double value, int base, int decimalPrecision = 8)
 	}
 
 	// Decimal part
-	std::cout << ansDecimal << std::endl;
+	std::cout << "Answer Decimal: " << ansDecimal << std::endl;
 
 	// Final answer
+	std::cout << "=======================================" << std::endl;
 	std::cout << answer << "." << ansDecimal << std::endl;
+}
+
+void baseToDecimal(long double value, int base, int decimalPrecision = 8)
+{
+	// For the integer part of value
+	int intValue = std::floor(value);
+	int intAns = 0;
+	for (int digit = 0; digit < std::floor(std::log10(intValue)); digit++)
+	{
+		int digitValue = static_cast<int>(std::floor(intValue / std::pow(10, digit))) % 10;  // Returns the digit from intValue
+		intAns += digitValue * static_cast<int>(std::pow(10, digit));
+		std::cout << digitValue << " * " << "10^" << digit << digitValue * std::pow(10, digit);
+	}
 }
 
 int main(int argc, char* argv[])
 {
-	for(int i = 0; i < argc; i++)
+	for(int i = 1; i < argc; i++)
 	{
 		std::string command = argv[i];
 		if (command == "--help")
 		{
 			std::cout << "-COMMANDS-\n" << 
-			"--decimalToBase [decimal] [base] -precision [precision] " << 
-			"| Converts a decimal to base, -precision can be ommited (default 8)\n";
-
+			"--dec2Base [decimal] [base] -p [precision] " << 
+			"| Converts a decimal to base, precision can be ommited (default 8)\n";
 		}
-		else if (command == "--decimalToBase")
+		else if (command == "--dec2Base")
 		{
-			if (argv[i + 3] == "-precision")
+			if (argv[i + 3] == "-p")
 			{
 				decimalToBase(std::stold(argv[i + 1]), std::stoi(argv[i + 2]), std::stoi(argv[i + 4]));	
 				i+= 4;
@@ -88,8 +101,13 @@ int main(int argc, char* argv[])
 			else
 			{
 				decimalToBase(std::stold(argv[i + 1]), std::stoi(argv[i + 2]));	
+				i+=2;
 			}
 			break;
+		}
+		else
+		{
+			std::cout << "Invalid Command: Type --help to view commands" << std::endl;
 		}
 	}
 }
